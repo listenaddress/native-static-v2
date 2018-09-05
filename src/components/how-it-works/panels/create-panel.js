3d9920b89c7a0a4a13fb0483b1ed490fb4c52267
oovg <office@vengist.com>
2018-09-05 08:57:40 -0700

    start tab panels styles

--- src/components/how-it-works/panels/create-panel.js
+++ src/components/how-it-works/panels/create-panel.js
@@ -7,12 +7,20 @@ class CreatePanel extends React.Component {
   render () {
     return (
       <div className="block">
-        <div className="contents">
+        <div className="contents contain">
           <div className="column">
-            <h2>HELLO Create</h2>
+            <h2>Create your community</h2>
+            <p>Your first step is to explore an choose communities that align with your values or things you care about. Put your money where your heart is.</p>
           </div>
           <div className="column">
             <h2>image</h2>
+          </div>
+          <div className="column">
+            <h2>image</h2>
+          </div>
+          <div className="column">
+            <h2>Your new currency</h2>
+            <p>Now that you are part of the lemonade community, you can track particapte in building the value of your Lemon Tokens. You can also use tokens to create tasks and fund projects.</p>
           </div>
         </div>
       </div>
