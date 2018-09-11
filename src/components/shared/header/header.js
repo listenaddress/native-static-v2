6b6ceb7d3ef72d8e6b8417a07a7ca1da19ce8196
skuhlmann <samkuhlmann@gmail.com>
2018-09-11 11:00:27 -0600

    Adds google analytics config and some outbound links

--- src/components/shared/header/header.js
+++ src/components/shared/header/header.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
+import Link from 'gatsby-link'
+
 import './header.scss'
 import Logo from '../../shared/logo/logo'
-import Link from 'gatsby-link'
 
 class Header extends React.Component {
   state = {
@@ -33,7 +35,9 @@ class Header extends React.Component {
             <Link to="/">Home</Link>
             <Link to="/how-it-works">How it works</Link>
             <Link to="/project">Project</Link>
-            <a className="button">Get Native Tokens</a>
+            <OutboundLink href="/" className="button">
+              Join Native
+            </OutboundLink>
           </div>
         </div>
         <button className={hamburgerClass} type="button" onClick={this.toggleNav}>
@@ -46,7 +50,9 @@ class Header extends React.Component {
             <Link to="/" onClick={this.toggleNav}>Home</Link>
             <Link to="/how-it-works" onClick={this.toggleNav}>How it works</Link>
             <Link to="/project" onClick={this.toggleNav}>Project</Link>
-            <a className="button" onClick={this.toggleNav}>Get Native Tokens</a>
+            <OutboundLink href="/" className="button" onClick={this.toggleNav}>
+              Join Native
+            </OutboundLink>
           </div>
         </div>
       </div>
