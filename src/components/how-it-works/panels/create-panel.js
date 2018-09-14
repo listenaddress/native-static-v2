e9514f0183a3345aa69a210f36216d38796562bd
oovg <office@vengist.com>
2018-09-14 09:37:05 -0700

    adds tab-panel style. needs active tab class on active button.

--- src/components/how-it-works/panels/create-panel.js
+++ src/components/how-it-works/panels/create-panel.js
@@ -6,20 +6,20 @@ class CreatePanel extends React.Component {
 
   render () {
     return (
-      <div className="block">
+      <div className="tab-panel block">
         <div className="contents contain">
           <div className="column">
-            <h2>Create your community</h2>
+            <h3>Create your community</h3>
             <p>Your first step is to explore an choose communities that align with your values or things you care about. Put your money where your heart is.</p>
           </div>
           <div className="column">
-            <h2>image</h2>
+            <p>image</p>
           </div>
           <div className="column">
-            <h2>image</h2>
+            <p>image</p>
           </div>
           <div className="column">
-            <h2>Your new currency</h2>
+            <h3>Your new currency</h3>
             <p>Now that you are part of the lemonade community, you can track particapte in building the value of your Lemon Tokens. You can also use tokens to create tasks and fund projects.</p>
           </div>
         </div>
