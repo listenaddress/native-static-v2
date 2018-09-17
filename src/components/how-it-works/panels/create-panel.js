e73590af34439834161716924ac2ab54aa393bbb
oovg <office@vengist.com>
2018-09-17 12:19:46 -0700

    adds active class to main-nav

--- src/components/how-it-works/panels/create-panel.js
+++ src/components/how-it-works/panels/create-panel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 import Illustration13 from '../../../assets/illustrations/13_Native_Website_Home_Howitworks_CREATE_TRIBE_V02.png'
 
 import './panel.scss'
@@ -8,14 +9,19 @@ class CreatePanel extends React.Component {
   render () {
     return (
       <div className="tab-panel block">
+
         <div className="contents contain">
+          <Fade left>
           <div className="column">
             <h3>Create your community</h3>
             <p>Your first step is to explore an choose communities that align with your values or things you care about. Put your money where your heart is.</p>
           </div>
+          </Fade>
+          <Fade right>
           <div className="column">
             <img src={Illustration13} />
           </div>
+          </Fade>
           <div className="column">
             <p>image</p>
           </div>
