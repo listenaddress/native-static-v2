2e83e7bdef4a25aeee743a13df7dcfd3d6b2c1ec
skuhlmann <samkuhlmann@gmail.com>
2018-09-18 13:37:09 -0600

    Adds lottie control component

---
+++ src/components/shared/lottie-control/lottie-control.js
@@ -0,0 +1,51 @@
+import React from "react"
+import Lottie from "react-lottie"
+import './lottie-control.scss';
+
+class LottieControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      defaultOptions: {
+        loop: this.props.loop,
+        autoplay: true,
+        animationData: this.props.animationData,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+      },
+      eventListeners: [{
+        eventName: 'complete',
+        callback: () => this.swapAnimation()
+      }]
+    }
+  }
+
+  swapAnimation = () => {
+    if (this.props.swapAnimationData) {
+      this.setState({
+        defaultOptions: {
+          loop: this.props.swapLoop,
+          autoplay: true,
+          animationData: this.props.swapAnimationData,
+          rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+          }
+        }
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Lottie options={this.state.defaultOptions} eventListeners={this.state.eventListeners} />
+      </div>
+    )
+  }
+
+}
+
+export default LottieControl;
