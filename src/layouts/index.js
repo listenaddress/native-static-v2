c498787e5e71dbb39cdf47c16fbd72d9b7596848
skuhlmann <samkuhlmann@gmail.com>
2018-11-05 18:29:13 -0700

    update script for new site

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -46,20 +46,6 @@ const Layout = ({ children, data }) => (
     </div>
     </ParallaxProvider>
     <Footer />
-
-    {/* <script
-        dangerouslySetInnerHTML={{
-          __html: `
-                  (function (d, t) {
-                    var pp = d.createElement(t), s = d.getElementsByTagName(t)[0];
-                    pp.src = '//app.pageproofer.com/overlay/js/3591/1855';
-                    pp.type = 'text/javascript';
-                    pp.async = true;
-                    s.parentNode.insertBefore(pp, s);
-                  })(document, 'script');
-                `,
-        }}
-      /> */}
   </div>
 )
 
--- static/scripts/page-proofer.js
+++ static/scripts/page-proofer.js
@@ -1,12 +1,9 @@
 (function (d, t) {
    var pp = d.createElement(t), s = d.getElementsByTagName(t)[0];
-
-   debugger
   //  pp.src = '//app.pageproofer.com/overlay/js/3590/1854';
-   pp.src = '//app.pageproofer.com/overlay/js/3591/1855';
+  //  pp.src = '//app.pageproofer.com/overlay/js/3591/1855';
+   pp.src = '//app.pageproofer.com/overlay/js/3594/1854';
    pp.type = 'text/javascript';
    pp.async = true;
-
    s.parentNode.insertBefore(pp, s);
-   console.log(s.parentNode)
 })(document, 'script');         
