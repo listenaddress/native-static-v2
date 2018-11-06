203ad0c64bddd03336f86e983e71e5a741c48fb1
skuhlmann <samkuhlmann@gmail.com>
2018-11-05 17:00:35 -0700

    Testing proofer

--- static/scripts/page-proofer.js
+++ static/scripts/page-proofer.js
@@ -1,7 +1,12 @@
 (function (d, t) {
    var pp = d.createElement(t), s = d.getElementsByTagName(t)[0];
-   pp.src = '//app.pageproofer.com/overlay/js/3590/1854';
+
+   debugger
+  //  pp.src = '//app.pageproofer.com/overlay/js/3590/1854';
+   pp.src = '//app.pageproofer.com/overlay/js/3591/1855';
    pp.type = 'text/javascript';
    pp.async = true;
+
    s.parentNode.insertBefore(pp, s);
+   console.log(s.parentNode)
 })(document, 'script');         
